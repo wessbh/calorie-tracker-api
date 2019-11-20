64b3817712f864112e35feb5db74ca4f3b03aa8d
Tarek <tarek.elghoul@outlook.com>
2019-11-20 19:42:43 +0100

    beta server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var passwordHash = require('password-hash');
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 var mysql = require('mysql');
+const cron = require('node-cron')
 var bodyparser = require('body-parser');
 var morgan = require('morgan');
 var cors = require("cors");
@@ -14,6 +15,15 @@ app.use('*', cors());
 app.use(morgan('dev'));
 var PORT =3000;
 var connection;
+
+
+var dateNow = new Date();
+var dd = dateNow.getDate();
+var monthSingleDigit = dateNow.getMonth() + 1,
+    mm = monthSingleDigit < 10 ? '0' + monthSingleDigit : monthSingleDigit;
+var yy = dateNow.getFullYear().toString().substr(2);
+
+var formattedDate = yy + '-' + mm + '-' + dd;
 
 calc_BMR(85, 183, 'female', 26, 1.2);
 //connect to database
@@ -67,7 +77,12 @@ app.post('/register', function (req, res) {
                 let selctSql = `SELECT * FROM user WHERE id = "${result.insertId}"`;
                 db.query(selctSql, (error, results) => {
                     if (results[0]) {
-                        var user = results[0];
+                        let row = {user_id:results[0].id, date:formattedDate};
+                        let sqlin = `INSERT INTO dailycal set ?`
+                        let query = db.query(sqlin, row, (err,result) => {
+                            if(err) throw err;
+                            console.log(result);
+                          })
                         res.status(200).send(results[0]);
                     } else {
                         res.status(400).send("Oops :( ");
@@ -142,15 +157,214 @@ function encrypt(text) {
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
-
+//get cateroies
 app.get('/categories',(req,res) => {
     let sql = `SELECT * FROM food_category`;
     let query = db.query(sql, (err,results) => {
         if(err) throw err;
         console.log(results);
-        res.send(results);
+        res.status(200).send({'categories': results});
     })
 });
+//get foodbycategories
+app.post('/foodbycategory',(req,res) => {
+    var category = req.body.category
+    let sqlid = `SELECT id as id FROM food_category WHERE category_name = '${category}'`
+    console.log(sqlid)
+    let query = db.query(sqlid, (err,results) => {
+        if(err) throw err;
+        console.log(results[0].id);
+        id = results[0].id
+        let sql = `SELECT * FROM food_calorie WHERE category = ${id}`;
+        let query = db.query(sql, (err,results) => {
+            if(err) throw err;
+            console.log(results);
+            res.status(200).send(results);
+        })
+    })
+    
+});
+//dailytracking
+app.post('/dailytrack', function (req, res) {
+    id = req.body.userid;
+    cal = req.body.calories;
+
+    let researchSql = `SELECT COUNT(*) as idCount FROM dailycal WHERE user_id = "${id}" AND date = '${formattedDate}'`;
+    db.query(researchSql, (error, results) => {
+        if (error) throw error;
+        if (results[0].idCount == 0) {
+            let row = {user_id:id, calories:cal, date:formattedDate};
+            let sql = 'INSERT INTO dailycal SET ?'
+            let query = db.query(sql, row, (err,result) => {
+              if(err) throw err;
+                console.log(result);
+                res.status(200).send({'message' : 'row added'});
+            })
+        } else {
+            let sql = `SELECT calories as cal FROM dailycal WHERE user_id = "${id}" AND date = '${formattedDate}'`;
+            let query = db.query(sql, (error,results) => {
+                if (error) throw error;
+                console.log('calorie from result: ', results[0].cal)
+                calorie = results[0].cal + parseFloat(cal);
+                console.log(calorie)
+                let sqlUpdate = `UPDATE dailycal SET calories = ${calorie} WHERE user_id = ${id} AND date = '${formattedDate}'`
+                console.log(sqlUpdate)
+                let query = db.query(sqlUpdate, (err,result) => {
+                    if(err) throw err;
+                    console.log(result);
+                    res.status(200).send({'message' : 'value updated'})
+                  })
+            })
+
+        }
+    });
+});
+//chart
+app.get('/getchartdata/:id',(req,res) => {
+    let sql = `SELECT  DATE_FORMAT(date, '%d/%m/%Y') as label, cal as y FROM saveddata WHERE user_id = ${req.params.id}`;
+    let query = db.query(sql, (err,results) => {
+        if(err) throw err;
+        console.log(JSON.stringify(results));
+        res.status(200).send(results)
+    })
+});
+
+
+
+cron.schedule("59 23 * * *",()=> {
+    let sql = `SELECT user_id as ids, calories as cal FROM dailycal WHERE date = '${formattedDate}'`
+    console.log(sql)
+    let query = db.query(sql, (error,results) => {
+        if(error) throw error;
+        console.log(results.length);
+        console.log(results[0].ids);
+        var i;
+        for(i = 0; i < results.length; i++){
+            id = results[i].ids;
+            cal = results[i].cal;
+            let row = {user_id:id, cal:cal, date:formattedDate};
+            let sqlin = `INSERT INTO saveddata set ?`
+            let query = db.query(sqlin, row, (err,result) => {
+                if(err) throw err;
+                console.log(result);
+              })
+
+        }
+    })
+})
+
+cron.schedule("0 0 * * *",()=> {
+    let sql = `SELECT id FROM user`
+    console.log(sql)
+    let query = db.query(sql, (error,results) => {
+        if(error) throw error;
+        console.log(results.length);
+        var i;
+        for(i = 0; i < results.length; i++){
+            id = results[i].id;
+            let row = {user_id:id, date:formattedDate};
+            let sqlin = `INSERT INTO dailycal set ?`
+            let query = db.query(sqlin, row, (err,result) => {
+                if(err) throw err;
+                console.log(result);
+              })
+
+        }
+    })
+})
+
+//get activity
+app.get('/activities/:id',(req,res) => {
+    let sql = `SELECT weight FROM user WHERE id = ${req.params.id}`
+    let query = db.query(sql, (err,results) => {
+        if(err) throw err;
+        userWeight = results[0].weight
+        console.log(userWeight)
+        var sqlActivity = ''
+        if(userWeight < 69){
+            sqlActivity = `SELECT name, bet57and69 as cal FROM activity_calorie`
+        } else if(70 <= userWeight && userWeight <85){
+            sqlActivity = `SELECT name, bet70and84 as cal FROM activity_calorie`
+        } else {
+            sqlActivity = `SELECT name, sup85 as cal FROM activity_calorie`
+        }
+        let query = db.query(sqlActivity, (err,results) => {
+            if(err) throw err;
+            res.status(200).send(results)
+        })
+    })
+});
+
+app.post('/addactivity',(req,res) => {
+
+    id = req.body.userid;
+    cal = req.body.calories;
+
+    let researchSql = `SELECT COUNT(*) as idCount FROM dailycal WHERE user_id = "${id}" AND date = '${formattedDate}'`;
+    console.log(formattedDate)
+    db.query(researchSql, (error, results) => {
+        if (error) throw error;
+        if (results[0].idCount == 0) {
+            let row = {user_id:id, activityCalorie:cal, date:formattedDate};
+            let sql = 'INSERT INTO dailycal SET ?'
+            let query = db.query(sql, row, (err,result) => {
+              if(err) throw err;
+                console.log(result);
+                res.status(200).send({'message' : 'row added'});
+            })
+        } else {
+            let sql = `SELECT activityCalorie as cal FROM dailycal WHERE user_id = "${id}" AND date = '${formattedDate}'`;
+            let query = db.query(sql, (error,results) => {
+                if (error) throw error;
+                console.log('calorie from result: ', results[0].cal)
+                calorie = results[0].cal + parseFloat(cal);
+                console.log("test",calorie)
+                let sqlUpdate = `UPDATE dailycal SET activityCalorie = ${calorie} WHERE user_id = ${id} AND date = '${formattedDate}'`
+                console.log(sqlUpdate)
+                let query = db.query(sqlUpdate, (err,result) => {
+                    if(err) throw err;
+                    console.log(result);
+                    res.status(200).send({'message' : 'value updated'})
+                  })
+            })
+
+        }
+    });
+});
+
+
+app.get('/getpourcentage/:id',(req,res) => {
+    let sql = `SELECT BMR FROM user WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err,results) => {
+        if(err) throw err;
+        var bmr = results[0].BMR
+        let sql1 = `SELECT calories,activityCalorie FROM dailycal WHERE user_id = ${req.params.id} AND date = '${formattedDate}'`;
+        let query1 = db.query(sql1, (err,results) => {
+        if(err) throw err;
+        var activityCal = results[0].activityCalorie;
+        console.log(activityCal);
+        var foodCal = results[0].calories;
+        console.log(foodCal)
+        if (foodCal - activityCal < 0){
+            console.log(activityCal - foodCal)
+            res.status(200).send({'pourcentage' : 0, 'BMR' : Math.round(bmr), 'FoodCal' : foodCal, 'ActivityCal' : activityCal});
+
+        } else if(foodCal - activityCal - bmr > 0) {
+            pourcentage = 100
+            res.status(200).send({'pourcentage' : 100, 'BMR' : Math.round(bmr), 'FoodCal' : foodCal, 'ActivityCal' : activityCal})
+        } else {
+            var pourcentage = ((foodCal - activityCal)/bmr)*100
+            console.log(pourcentage)
+            res.status(200).send({'pourcentage' : Math.round(pourcentage), 'BMR' : Math.round(bmr), 'FoodCal' : foodCal, 'ActivityCal' : activityCal})
+        }
+        })
+    })
+    
+});
+
+
+
+
 
 function decrypt(text) {
     let iv = Buffer.from(text.iv, 'hex');
@@ -191,7 +405,7 @@ function calc_BMR (weight, height, gender, age, frequence_activity){
     var bmr = (weight * 10) + (6.25 * 185) - (age *5) +gender_value;
     var final_bmr = bmr * frequence_activity
     console.log(final_bmr);
-    return final_bmr;
+    return Math.round(final_bmr);
 }
 
 function calc_BMI(w, h) {
